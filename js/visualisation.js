85f8ed3fd00a86f3b847edc6380e8b888fc74819
Unknown <egle.katilauskaite@gmail.com>
2019-02-18 00:47:08 +0000

    Add a tooltip

--- js/visualisation.js
+++ js/visualisation.js
@@ -5,54 +5,97 @@
 
  	console.log("done");
  });
-d3.queue()
-	.defer(d3.json, "data/world-countries.json")
-	.defer(d3.csv, "data/final_data.csv")
-	.await(ready);
+ d3.queue()
+ .defer(d3.json, "data/world-countries.json")
+ .defer(d3.csv, "data/final_data.csv", 
+		//clean data
+		function(data){
+			//if some coordinates are missing, ignore it
+			if(data["latitude"] !== "" && data["longitude"]!=="")
+				return data;
+
+		})
+ .await(ready);
 
 
  var width = 1300;
  var height = 900;
 
-var map = d3.select("div#d3-visualisation").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+ var map = d3.select("div#d3-visualisation").append("svg")
+ .attr("width", width)
+ .attr("height", height);
+
+ var div = d3.select("#d3-visualisation").append("div")	
+ .attr("class", "tooltip")				
+ .style("opacity", 0);
 
 
-var scale = 200;
+ var scale = 200;
+ var radius = 3;
+ var projection = d3.geoMercator()
+ .translate([width/2,height/2])
+ .scale(scale);
 
-var projection = d3.geoMercator()
-    .translate([width/2,height/2])
-    .scale(scale);
-
-var path = d3.geoPath().projection(projection);
+ var path = d3.geoPath().projection(projection);
 
 
 
-function ready(error,worldData,data){
+ function ready(error,worldData,data){
 
-	var countries = topojson.feature(worldData, worldData.objects.countries1).features
+ 	var countries = topojson.feature(worldData, worldData.objects.countries1).features
 
-	map.selectAll(".country")
-		.data(countries)
-		.enter().append("path")
-		.attr("class", "country")
-		.attr("d",path);
+ 	map.selectAll(".country")
+ 	.data(countries)
+ 	.enter().append("path")
+ 	.attr("class", "country")
+ 	.attr("d",path);
 
-	map.selectAll(".power-plant")
-		.data(data)
-		.enter().append("circle")
-		.attr("r",2)
-		.attr("cx", function(d){
-			var coords = projection([d.longitude, d.latitude]);
-			return coords[0];
-		})
-		.attr("cy", function(d) {
-			var coords = projection([d.longitude, d.latitude]);
-			return coords[1];
+ 	map.selectAll(".power-plant")
+ 	.data(data)
+ 	.enter().append("circle")
+ 	.attr("r",radius)
+ 	.attr("cx", function(d){
+ 		var coords = projection([d.longitude, d.latitude]);
+ 		return coords[0];
+ 	})
+ 	.attr("cy", function(d) {
+ 		var coords = projection([d.longitude, d.latitude]);
+ 		return coords[1];
+ 	})
+ 	.attr("fill", function(d) {
+			return retrieveFuelColor(d["fuel1"]);
+	})
+		//tooltip
+	.on("mouseover", function(d,e,f,g) {	
+			//change size
+            d3.select(this).attr("r",radius * 5);
+
+			div.transition()		
+			.duration(200)		
+			.style("opacity", .9);		
+			div	.html("Built in " + d["country_long"] + "<br>" + d["name"] + "<br>Main fuel type: " + d["fuel1"]);
+	})					
+	.on("mouseout", function(d) {
+	//change size
+             d3.select(this).attr("r",radius);
+	
+			div.transition()		
+			.duration(500)		
+			.style("opacity", 0);	
 		});
 
+	}
 
 
-	console.log(data);
+
+function retrieveFuelColor(fuel) {
+	//bad
+	if (fuel === "Coal" ||fuel === "Gas" || fuel === "Oil")
+		return "red";
+	//good
+	else if (fuel === "Geothermal" || fuel=== "Hydro" || fuel=== "Solar" || fuel === "Wave and Tidal" || fuel === "Wind")
+		return "green";
+	//neutral
+	else if (fuel === "Nuclear" || fuel === "Waste" || fuel === "Other" || fuel === "Biomass")
+		return "yellow";
 }
